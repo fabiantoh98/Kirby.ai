ab3fa8d6b72744c1f70c48d2e2abad310f27048e
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-02-15 19:02:21 +0800

    Add load ingredients and load meals methods

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pytesseract
 from PIL import Image
-
+from google import genai
+from google.genai import types
 import pandas as pd
 import numpy as np
+
 # ----- PAGE CONFIG & STYLES -----
 st.set_page_config(
     page_title="Kirby.AI",
@@ -62,14 +64,33 @@ def main():
 def page_1():
     st.subheader("Food Recommender")
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
     if uploaded_file is not None:
         image = np.array(Image.open(uploaded_file))
         st.image(image, caption='Uploaded Image.', use_container_width=True)
         st.write("")
         with st.spinner("Extracting text..."):
+            
+            @st.cache_data
+            def load_ingredients():
+                return pd.read_json('ingredients.json')
+            
+            @st.cache_data
+            def load_meals():
+                return pd.read_json('big_data.json')
+            
+            client = genai.Client(api_key="")
             text = pytesseract.image_to_string(image)
-        if text.strip():
-            st.text_area("Extracted Text", text, height=200)
+            ingredients = load_ingredients()
+            meals = load_meals()
+            response = client.models.generate_content(
+                model="gemini-2.0-flash", contents=[text, ingredients.to_string(), meals.to_string(), "Obtain the food items from the image then cross reference with the ingredients list and filter only the food items that exist in the ingredients list and get the ingredient list name. After that, obtain the strMeal's that matches the ingredients best and return it. Then returned object should just contain the strMeals's + included ingredients + excluded ingredients and nothing else. If none then say none."]
+            )
+            print(response.text)
+        if response.text.strip():
+            # ingredients = load_ingredients()
+            # st.write(ingredients)
+            st.text_area("Extracted Text", response.text, height=200)
         else:
             st.write("No text found in the image.")
 
